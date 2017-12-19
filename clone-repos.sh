5bd6f2c6e9518461b6900b72033294e269512f92
Jonathan Giles <jonathan@jonathangiles.net>
2017-12-19 16:25:06 +1300

    Updated to add repos that were tested locally and work

--- clone-repos.sh
+++ clone-repos.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+# Compile fine
 git clone https://github.com/Azure/azure-functions-java-worker.git
 git clone https://github.com/Azure/azure-service-bus-java.git
-git clone https://github.com/Azure/azure-storage-java.git
-#git clone https://github.com/Azure/azure-libraries-for-java.git
 git clone https://github.com/Azure/azure-data-lake-store-java.git
-git clone https://github.com/Azure/azure-keyvault-java.git
+git clone https://github.com/Azure/azure-event-hubs-java.git
+git clone https://github.com/Azure/azure-iot-sdk-java.git
 
-#git clone https://github.com/Azure/azure-iot-sdk-java.git
-##git clone https://github.com/Azure/azure-documentdb-java.git
-#git clone https://github.com/Azure/azure-event-hubs-java.git
+# Untested
+
+
+# Repos that fail to compile
+# git clone https://github.com/Azure/azure-keyvault-java.git
+# git clone https://github.com/Azure/azure-libraries-for-java.git
+
+# Repos with JavaDoc errors that fail build
+#git clone https://github.com/Azure/azure-storage-java.git
+#git clone https://github.com/Azure/azure-batch-sdk-for-java.git
