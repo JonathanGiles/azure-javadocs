e0fcf57dd81ac554b21ef50fcd667b12c5dabb7f
Jonathan Giles <jonathan@jonathangiles.net>
2017-12-19 14:48:18 +1300

    Update clone-repos.sh

--- clone-repos.sh
+++ clone-repos.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/Azure/azure-functions-java-worker.git
 git clone https://github.com/Azure/azure-service-bus-java.git
 git clone https://github.com/Azure/azure-storage-java.git
-git clone https://github.com/Azure/azure-libraries-for-java.git
+#git clone https://github.com/Azure/azure-libraries-for-java.git
 git clone https://github.com/Azure/azure-data-lake-store-java.git
 git clone https://github.com/Azure/azure-keyvault-java.git
 
