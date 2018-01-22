a1d3cce0c1ca55f6bf20ab9596db0fe6d9910fe4
Jonathan Giles <jonathan@jonathangiles.net>
2018-01-23 11:33:11 +1300

    Add support for azure-keyvault-java and azure-batch-sdk-for-java, now that these two projects have merged their respective pull requests I raised.

--- clone-repos.sh
+++ clone-repos.sh
@@ -5,6 +5,8 @@ git clone https://github.com/Azure/azure-functions-java-worker.git
 git clone https://github.com/Azure/azure-service-bus-java.git
 git clone https://github.com/Azure/azure-data-lake-store-java.git
 git clone https://github.com/Azure/azure-event-hubs-java.git
+git clone https://github.com/Azure/azure-keyvault-java.git
+git clone https://github.com/Azure/azure-batch-sdk-for-java.git
 
 # Fails to build under JDK 9
 # git clone https://github.com/Azure/azure-iot-sdk-java.git
@@ -13,10 +15,8 @@ git clone https://github.com/Azure/azure-event-hubs-java.git
 
 
 # Repos that fail to compile
-# git clone https://github.com/Azure/azure-keyvault-java.git
 # git clone https://github.com/Azure/azure-libraries-for-java.git
 
 # Repos with JavaDoc errors that fail build
 # git clone https://github.com/Azure/azure-storage-java.git
-# git clone https://github.com/Azure/azure-batch-sdk-for-java.git
 # git clone https://github.com/Azure/azure-sdk-for-java.git
