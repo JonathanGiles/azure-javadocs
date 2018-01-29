73362d780ce13751b5599a19b12eea18d061b5e1
Jonathan Giles <jonathan@jonathangiles.net>
2018-01-29 16:49:18 +1300

    Generate jar and attempt to push to github also. Also, disable some repos as they are now failing to build :-(

--- clone-repos.sh
+++ clone-repos.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Compile fine
 git clone https://github.com/Azure/azure-functions-java-worker.git
 git clone https://github.com/Azure/azure-service-bus-java.git
 git clone https://github.com/Azure/azure-data-lake-store-java.git
@@ -9,17 +8,6 @@ git clone https://github.com/Azure/azure-keyvault-java.git
 git clone https://github.com/Azure/azure-batch-sdk-for-java.git
 git clone https://github.com/Azure/azure-storage-java.git
 git clone https://github.com/Azure/azure-iot-sdk-java.git
-
-
-# Fails to build under JDK 9
-
-
-# Untested
-
-
-# Repos that fail to compile
-# git clone https://github.com/Azure/azure-libraries-for-java.git
-
-# Repos with JavaDoc errors that fail build
-
-# git clone https://github.com/Azure/azure-sdk-for-java.git
+git clone https://github.com/AzureAD/azure-activedirectory-library-for-java.git
+git clone https://github.com/Azure/azure-libraries-for-java.git
+git clone https://github.com/Azure/azure-sdk-for-java.git
