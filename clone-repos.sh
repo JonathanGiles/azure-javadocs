02038a2626ace41c504915fb00230362d1e07d7a
Jonathan Giles <jonathan@jonathangiles.net>
2018-08-20 11:41:39 +1200

    Cleaning up repo - moving back to hosting the build scripts here (but hosting remains on Azure Storage)

--- clone-repos.sh
+++ clone-repos.sh
@@ -4,10 +4,10 @@ git clone https://github.com/Azure/azure-functions-java-library.git
 git clone https://github.com/Azure/azure-service-bus-java.git
 git clone https://github.com/Azure/azure-data-lake-store-java.git
 git clone https://github.com/Azure/azure-event-hubs-java.git
-git clone https://github.com/JonathanGiles/azure-keyvault-java.git
+git clone https://github.com/Azure/azure-keyvault-java.git
 git clone https://github.com/Azure/azure-batch-sdk-for-java.git
 git clone https://github.com/Azure/azure-storage-java.git
-git clone https://github.com/Azure/azure-iot-sdk-java.git
+git clone https://github.com/JonathanGiles/azure-iot-sdk-java.git
 git clone https://github.com/AzureAD/azure-activedirectory-library-for-java.git
 git clone https://github.com/Azure/azure-libraries-for-java.git
 git clone https://github.com/Azure/azure-sdk-for-java.git
