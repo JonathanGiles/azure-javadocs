0881a3d0d40385a7e98e26d38d1f6a0ca28e2a8b
Jonathan Giles <jonathan@jonathangiles.net>
2018-08-21 10:29:21 +1200

    Pull request into azure-iot-sdk-java repo was merged, so moving the clone script to use the official repo.

--- clone-repos.sh
+++ clone-repos.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Azure/azure-event-hubs-java.git
 git clone https://github.com/Azure/azure-keyvault-java.git
 git clone https://github.com/Azure/azure-batch-sdk-for-java.git
 git clone https://github.com/Azure/azure-storage-java.git
-git clone https://github.com/JonathanGiles/azure-iot-sdk-java.git
+git clone https://github.com/Azure/azure-iot-sdk-java.git
 git clone https://github.com/AzureAD/azure-activedirectory-library-for-java.git
 git clone https://github.com/Azure/azure-libraries-for-java.git
 git clone https://github.com/Azure/azure-sdk-for-java.git
