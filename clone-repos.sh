5d1656b871de5efa7c0876408ef9c11dfe556e75
Jonathan Giles <jonathan@jonathangiles.net>
2018-08-28 10:44:02 +1200

    Using my fork and branch for azure storage

--- clone-repos.sh
+++ clone-repos.sh
@@ -6,7 +6,7 @@ git clone --depth 1 https://github.com/JonathanGiles/azure-data-lake-store-java.
 git clone --depth 1 https://github.com/Azure/azure-event-hubs-java.git
 git clone --depth 1 https://github.com/Azure/azure-keyvault-java.git
 git clone --depth 1 https://github.com/Azure/azure-batch-sdk-for-java.git
-git clone --depth 1 https://github.com/JonathanGiles/azure-storage-java.git
+git clone --depth 1 --branch jdk11-support https://github.com/JonathanGiles/azure-storage-java.git
 git clone --depth 1 --branch jdk11-support https://github.com/JonathanGiles/azure-iot-sdk-java.git
 git clone --depth 1 https://github.com/AzureAD/azure-activedirectory-library-for-java.git
 git clone --depth 1 https://github.com/Azure/azure-libraries-for-java.git
