ef63a93d1c7891d9d9752e16a35cc4b05f6f433d
Jonathan Giles <jonathan@jonathangiles.net>
2018-08-28 12:00:30 +1200

    Use my fork and branch of azure-libraries-for-java

--- clone-repos.sh
+++ clone-repos.sh
@@ -9,6 +9,6 @@ git clone --depth 1 https://github.com/Azure/azure-batch-sdk-for-java.git
 git clone --depth 1 --branch jdk11-support https://github.com/JonathanGiles/azure-storage-java.git
 git clone --depth 1 --branch jdk11-support https://github.com/JonathanGiles/azure-iot-sdk-java.git
 git clone --depth 1 https://github.com/AzureAD/azure-activedirectory-library-for-java.git
-git clone --depth 1 https://github.com/Azure/azure-libraries-for-java.git
+git clone --depth 1 --branch javadoc-fixes https://github.com/JonathanGiles/azure-libraries-for-java.git
 git clone --depth 1 https://github.com/Azure/azure-sdk-for-java.git
 git clone --depth 1 https://github.com/JonathanGiles/azure-cosmosdb-java.git
